562a1fdbaf61fdc525d2a4868c3024f872578680
Frederico Araujo <fredericoaraujo@gmail.com>
2021-06-10 22:31:55 -0300

    Added orders' test

--- test/orders/order_test.exs
+++ test/orders/order_test.exs
@@ -1,2 +1,61 @@
 defmodule Exlivery.Orders.OrderTest do
+  use ExUnit.Case
+
+  import Exlivery.Factory
+
+  alias Exlivery.Orders.Order
+
+  describe "build/2" do
+    test "when all params is corrected, returns an order" do
+      user = build(:user)
+
+      items = [
+        build(:item),
+        build(:item,
+          description: "Temaki",
+          category: :japonesa,
+          quantity: 2,
+          unity_price: Decimal.new("20.5")
+        )
+      ]
+
+      response = Order.build(user, items)
+
+      expected = {:ok, build(:order)}
+
+      assert response == expected
+    end
+
+    test "when items is nil, returns an error" do
+      user = build(:user)
+
+      items = []
+
+      response = Order.build(user, items)
+
+      expected = {:error, "Order is invalid!"}
+
+      assert response == expected
+    end
+
+    test "when user is nil, returns an error" do
+      user = nil
+
+      items = [
+        build(:item),
+        build(:item,
+          description: "Temaki",
+          category: :japonesa,
+          quantity: 2,
+          unity_price: Decimal.new("20.5")
+        )
+      ]
+
+      response = Order.build(user, items)
+
+      expected = {:error, "Order is invalid!"}
+
+      assert response == expected
+    end
+  end
 end
