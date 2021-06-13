6df344635c9ddeb8a029451ad38ccf235dacf880
Frederico Araujo <fredericoaraujo@gmail.com>
2021-06-13 12:54:18 -0300

    Created save orders

--- lib/exlivery.ex
+++ lib/exlivery.ex
@@ -1,10 +1,14 @@
 defmodule Exlivery do
+  alias Exlivery.Orders.Agent, as: OrderAgent
+  alias Exlivery.Orders.CreteOrUpdate, as: OrderCreateOrUpdate
   alias Exlivery.Users.Agent, as: UserAgent
-  alias Exlivery.Users.CreteOrUpdate
+  alias Exlivery.Users.CreteOrUpdate, as: UserCreateOrUpdate
 
   def start_links do
     UserAgent.start_link(%{})
+    OrderAgent.start_link(%{})
   end
 
-  defdelegate create_or_update_user(params), to: CreteOrUpdate, as: :call
+  defdelegate create_or_update_user(params), to: UserCreateOrUpdate, as: :call
+  defdelegate create_or_update_order(params), to: OrderCreateOrUpdate, as: :call
 end
